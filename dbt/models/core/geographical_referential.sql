80c8fbc8b3c4076dec752476ba610d6fc6f83d4c
Asma-Chloë Farah <asmachloe@gmail.com>
2024-04-15 01:48:19 +0000

    graography column added to referential

--- dbt/models/core/geographical_referential.sql
+++ dbt/models/core/geographical_referential.sql
@@ -1,5 +1,10 @@
 {{ config(materialized='table') }}
 
 select 
-    *
+    * ,
+    ST_GeogFromGeoJson(
+    CONCAT(
+      '{"type":"', geo_shape_geometry_type, '","coordinates":', geo_shape_geometry_coordinates, '}'
+    )
+  ) AS geography_column
 from {{ ref('geographical_referential_raw')}}
